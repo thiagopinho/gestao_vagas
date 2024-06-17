df0281869bcc1187de9e4879c64f5e9b2a030003
Thiago Pinho <thiagopinho05@gmail.com>
2024-06-17 11:47:52 -0300

    ✨ feat: inserting validations in fields

--- src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,13 +2,23 @@ package br.com.thiagopinho.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class CandidateEntity {
     private UUID id;
+
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Name should contain only letters")
     private String name;
+
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Length(min = 6, message = "Password should have at least 6 characters")
     private String password;
     private String username;
     private String description;
