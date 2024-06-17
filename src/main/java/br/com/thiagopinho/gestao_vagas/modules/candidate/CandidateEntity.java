668d78b42853b3e1d0295d70037a2cfd6256dbf8
Thiago Pinho <thiagopinho05@gmail.com>
2024-06-17 19:22:20 -0300

    ✨ feat: creating exception handler and DTO to error messages from API

--- src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,18 +1,27 @@
 package br.com.thiagopinho.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+@Entity(name = "candidate")
 @Data
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Name should contain only letters")
     private String name;
 
     @Email(message = "Email should be valid")
@@ -20,7 +29,13 @@ public class CandidateEntity {
 
     @Length(min = 6, message = "Password should have at least 6 characters")
     private String password;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "Username should not have spaces")
     private String username;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
