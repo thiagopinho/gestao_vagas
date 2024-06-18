1cea6658b8fa298172dda7fdb83a00fe1204c4f1
Thiago Pinho <thiagopinho05@gmail.com>
2024-06-18 12:01:40 -0300

    ⚡️ feat: creating UseCase and passing the controller logic to it.

--- src/main/java/br/com/thiagopinho/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/br/com/thiagopinho/gestao_vagas/exceptions/UserFoundException.java
@@ -2,6 +2,6 @@ package br.com.thiagopinho.gestao_vagas.exceptions;
 
 public class UserFoundException extends RuntimeException {
     public UserFoundException() {
-        super("Usuário já existe!");
+        super("Usuário ou e-mail já existe!");
     }
 }
