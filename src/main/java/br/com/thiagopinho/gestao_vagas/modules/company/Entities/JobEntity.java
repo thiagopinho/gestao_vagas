1d21b26bdcf328ad187d604977f02a3728b0c005
Thiago Pinho <thiagopinho05@gmail.com>
2024-06-19 18:37:32 -0300

    🚀 feat: creating Job controllers, entity and function to useCase.

--- src/main/java/br/com/thiagopinho/gestao_vagas/modules/company/Entities/JobEntity.java
+++ src/main/java/br/com/thiagopinho/gestao_vagas/modules/company/Entities/JobEntity.java
@@ -5,13 +5,12 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity(name = "job")
@@ -22,16 +21,18 @@ public class JobEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    private String level;
-    private String benefits;
+    @NotBlank(message = "Esse campo é obrigatório!")
     private String description;
 
-    @ManyToOne()
-    @JoinColumn(name = "company_id", insertable = false, updatable = false)
-    private CompanyEntity companyEntity;
+    private String benefits;
+    private String level;
 
-    @Column(name = "company_id")
-    private UUID companyId;
+    // @ManyToOne()
+    // @JoinColumn(name = "company_id", insertable = false, updatable = false)
+    // private CompanyEntity companyEntity;
+
+    // @Column(name = "company_id", nullable = false)
+    // private UUID companyId;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
