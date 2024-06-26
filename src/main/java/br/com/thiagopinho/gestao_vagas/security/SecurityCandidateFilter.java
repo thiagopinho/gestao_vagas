03af31167e8e11b1985fedccd438374cd0c9ce0d
Thiago Pinho <thiagopinho05@gmail.com>
2024-06-26 19:03:46 -0300

    feat: creating a different JWT token to candidate and putting condition on security filter

--- src/main/java/br/com/thiagopinho/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/thiagopinho/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,51 +1,49 @@
 package br.com.thiagopinho.gestao_vagas.security;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import br.com.thiagopinho.gestao_vagas.providers.JWTProvider;
+import br.com.thiagopinho.gestao_vagas.providers.JWTCandidateProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter {
+public class SecurityCandidateFilter extends OncePerRequestFilter {
 
     @Autowired
-    private JWTProvider jwtProvider;
+    private JWTCandidateProvider jwtCandidateProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-            HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
         SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
-        if (request.getRequestURI().startsWith("/company")) {
+        if (request.getRequestURI().startsWith("/candidate")) {
             if (header != null) {
-                var subjectToken = this.jwtProvider.validateToken(header);
-                if (subjectToken.isEmpty()) {
+                var token = this.jwtCandidateProvider.validateToken(header);
+
+                if (token == null) {
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                     return;
                 }
-                request.setAttribute("company_id", subjectToken);
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null,
-                        Collections.emptyList());
-                SecurityContextHolder.getContext().setAuthentication(auth);
+
+                request.setAttribute("candidate_id", token.getSubject());
+                System.out.println("TOKEN ============: " + token);
+                System.out.println("TOKEN subject ============: " + token.getSubject());
+                return;
             }
 
         }
 
         filterChain.doFilter(request, response);
-
     }
 
 }
