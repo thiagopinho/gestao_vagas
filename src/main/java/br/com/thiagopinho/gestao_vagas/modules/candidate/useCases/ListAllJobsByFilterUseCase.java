714815d439a0009ac5ce4d4cab3a9299871e12ad
Thiago Pinho <thiagopinho05@gmail.com>
2024-06-30 18:58:57 -0300

    ⚡️ feat: start a creation of list from all jobs for candidate

---
+++ src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
@@ -0,0 +1,20 @@
+package br.com.thiagopinho.gestao_vagas.modules.candidate.useCases;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.thiagopinho.gestao_vagas.modules.company.Entities.JobEntity;
+import br.com.thiagopinho.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class ListAllJobsByFilterUseCase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public List<JobEntity> execute(String filter) {
+        return this.jobRepository.findByDescriptionContaining(filter);
+    }
+}
