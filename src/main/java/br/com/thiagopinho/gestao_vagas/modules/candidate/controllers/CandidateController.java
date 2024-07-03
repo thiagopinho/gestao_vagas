3d63a0746f200f7a6499521089995ef022f66649
Thiago Pinho <thiagopinho05@gmail.com>
2024-07-03 19:03:52 -0300

    📝 doc: defining error return

--- src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.thiagopinho.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.thiagopinho.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.thiagopinho.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import br.com.thiagopinho.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.thiagopinho.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
@@ -57,6 +58,17 @@ public class CandidateController {
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidate", description = "Informations about candidate")
+    @Operation(summary = "User profile", description = "Function to get user profile")
+    @SecurityRequirement(name = "jwt_auth")
+    @ApiResponses({
+
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = ProfileCandidateResponseDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Error to show user profile")
+
+    })
     public ResponseEntity<Object> get(HttpServletRequest request) {
 
         var idCandidate = request.getAttribute("candidate_id");
--- src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/thiagopinho/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,15 +2,29 @@ package br.com.thiagopinho.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProfileCandidateResponseDTO {
+
+    @Schema(example = "Python Developer")
     private String description;
+
+    @Schema(example = "user@gmail.com")
     private String email;
+
+    @Schema(example = "Jhon")
     private String username;
+
+    @Schema(example = "Jhon Scott")
     private String name;
+
     private UUID id;
 }
